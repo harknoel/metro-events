03efb7db522de00a6378cdf01f1db593401d52ff
harknoel <bontilao.harknoel@gmail.com>
2024-03-25 10:36:00 +0800

    Fix event user's container

--- src/components/UserEvents.js
+++ src/components/UserEvents.js
@@ -5,61 +5,64 @@ import Container from "./styles/Container.styled";
 import axios from "axios";
 import { UserContext } from "../App";
 import { useNavigate } from "react-router-dom";
+import { CardContainer } from "./styles/UserEvents.styled";
 
 const UserEvents = () => {
-	const [open, setOpen] = useState(false);
-	const [events, setEvents] = useState();
-	const { user, setUser } = useContext(UserContext);
+  const [open, setOpen] = useState(false);
+  const [events, setEvents] = useState();
+  const { user, setUser } = useContext(UserContext);
 
-	let role = null;
-	if (user !== null) {
-		role = user.user.authorities[0].authority;
-	}
+  let role = null;
+  if (user !== null) {
+    role = user.user.authorities[0].authority;
+  }
 
-	const navigate = useNavigate();
-	useEffect(() => {
-		if (user == null || (role !== "USER" && role !== "ORGANIZER")) {
-			navigate("/signin");
-		}
-	}, [user, navigate]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user == null || (role !== "USER" && role !== "ORGANIZER")) {
+      navigate("/signin");
+    }
+  }, [user, navigate]);
 
-	console.log(localStorage.getItem("username"));
+  console.log(localStorage.getItem("username"));
 
-	const getAllEvent = async () => {
-		try {
-			const response = await axios.get("/users/allEvents");
-			setEvents(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	useEffect(() => {
-		console.log(events);
-	}, [events]);
+  const getAllEvent = async () => {
+    try {
+      const response = await axios.get("/users/allEvents");
+      setEvents(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    console.log(events);
+  }, [events]);
 
-	useEffect(() => {
-		getAllEvent();
-	}, []);
+  useEffect(() => {
+    getAllEvent();
+  }, []);
 
-	return (
-		<div>
-			<UserNav />
-			<Container>
-				<h1>My Joined Events</h1>
-				{events &&
-					events.map((event) => (
-						<Event
-							key={event.eventId}
-							event={event}
-							open={open}
-							setOpen={setOpen}
-							showJoin={true}
-							showManage={true}
-						/>
-					))}
-			</Container>
-		</div>
-	);
+  return (
+    <div>
+      <UserNav />
+      <Container>
+        <h1>My Joined Events</h1>
+        <CardContainer>
+          {events &&
+            events.map((event) => (
+              <Event
+                key={event.eventId}
+                event={event}
+                open={open}
+                setOpen={setOpen}
+                showJoin={true}
+                showManage={true}
+              />
+            ))}
+        </CardContainer>
+      </Container>
+    </div>
+  );
 };
 
 export default UserEvents;
