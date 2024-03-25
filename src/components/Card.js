38acc7e319f5eba7d36b2d63bf5e8424c51a509e
harknoel <bontilao.harknoel@gmail.com>
2024-03-25 10:26:43 +0800

    Fix user events display

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,42 +1,40 @@
+import React from "react";
 import { StyledCard, Title, Description } from "./styles/Card.styled";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+
 const Card = (props) => {
-	const { event, setOpen } = props;
+  const { event, handleOpen } = props;
 
-	const handleOpen = () => {
-		setOpen(true);
-	};
-
-	return (
-		<div>
-			<StyledCard>
-				<CardContent>
-					<Title gutterBottom variant="h5" component="div">
-						{event.title}
-					</Title>
-					<Typography variant="body2" color="text.secondary">
-						<strong>Organizer:</strong> {event.owner}
-						<br />
-						<strong>Date Started:</strong> {event.dateStart}
-						<br />
-						<strong>Time Started:</strong> {event.timeStart}
-						<br />
-					</Typography>
-					<Description variant="body2" color="text.secondary">
-						{event.description}
-					</Description>
-				</CardContent>
-				<CardActions>
-					<Button onClick={handleOpen} size="small">
-						Learn More
-					</Button>
-				</CardActions>
-			</StyledCard>
-		</div>
-	);
+  return (
+    <div>
+      <StyledCard>
+        <CardContent>
+          <Title gutterBottom variant="h5" component="div">
+            {event.title}
+          </Title>
+          <Typography variant="body2" color="text.secondary">
+            <strong>Organizer:</strong> {event.owner}
+            <br />
+            <strong>Date Started:</strong> {event.dateStart}
+            <br />
+            <strong>Time Started:</strong> {event.timeStart}
+            <br />
+          </Typography>
+          <Description variant="body2" color="text.secondary">
+            {event.description}
+          </Description>
+        </CardContent>
+        <CardActions>
+          <Button onClick={handleOpen} size="small">
+            Learn More
+          </Button>
+        </CardActions>
+      </StyledCard>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,24 +1,32 @@
-import { CardContainer } from "./styles/UserEvents.styled";
-import Card from "./Card";
+import React, { useState } from "react";
 import CustomModal from "./CustomModal";
+import Card from "./Card";
 
 const Event = (props) => {
-	const { event, open, setOpen, showJoin, showManage } = props;
+  const { event, showJoin, showManage } = props;
+  const [open, setOpen] = useState(false);
 
-	return (
-		<div>
-			<CardContainer>
-				<Card event={event} setOpen={setOpen} />
-			</CardContainer>
-			<CustomModal
-				event={event}
-				open={open}
-				setOpen={setOpen}
-				showJoin={showJoin}
-				showManage={showManage}
-			/>
-		</div>
-	);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Card event={event} handleOpen={handleOpen} />
+      <CustomModal
+        event={event}
+        open={open}
+        handleClose={handleClose}
+        showJoin={showJoin}
+        showManage={showManage}
+        setOpen={setOpen}
+      />
+    </div>
+  );
 };
 
 export default Event;
