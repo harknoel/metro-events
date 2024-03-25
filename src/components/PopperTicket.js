9ed7adfc1c390437beda7c0f05565180d753922b
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-26 01:35:00 +0800

    Implement request role to become organizer

--- src/components/PopperTicket.js
+++ src/components/PopperTicket.js
@@ -2,20 +2,37 @@ import React from "react";
 import Popper from "@mui/material/Popper";
 import Fade from "@mui/material/Fade";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
-import {
-	StyledPopperContent,
-	RequestAdminButton,
-	RequestOrganizerButton,
-} from "./styles/PopperTicket.styled";
+import { StyledPopperContent } from "./styles/PopperTicket.styled";
 import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { Grid } from "@mui/material";
+import axiosInstance from "../config/axiosInstance";
 
 const PopperTicket = (props) => {
 	const { bindToggle, bindPopper, popupState } = props;
+
+	const requestRole = async () => {
+		try {
+			const response = await axiosInstance.post(
+				"users/request",
+				localStorage.getItem("username")
+			);
+			if (parseInt(response.data) === 1) {
+				alert("Request sent successfully.");
+			} else {
+				alert("Already sent request.");
+			}
+		} catch (error) {
+			console.log("error");
+		}
+	};
+
+	const handleSubmit = () => {
+		requestRole();
+	};
 
 	return (
 		<div>
@@ -34,7 +51,11 @@ const PopperTicket = (props) => {
 											Request to be an organizer.
 										</Typography>
 										<Grid item xs={2}>
-											<Button size="small" color="primary">
+											<Button
+												onClick={handleSubmit}
+												size="small"
+												color="primary"
+											>
 												Request
 											</Button>
 										</Grid>
