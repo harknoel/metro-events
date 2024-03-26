02564d8ab4aef96ff864a3888a17cd318cf9c0bc
Topitops69 <topitops123@gmail.com>
2024-03-26 16:45:27 +0800

    Notifications not complete

--- src/components/NotificationPopup.js
+++ src/components/NotificationPopup.js
@@ -1,13 +1,35 @@
-import { Divider, List, ListItem, ListItemText } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Popper from "@mui/material/Popper";
 import Fade from "@mui/material/Fade";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 import { StyledNotificationPopup } from "./styles/NotificationPopup.styled";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
-const NotificationPopup = (props) => {
-  const { notifications, bindToggle, bindPopper, popupState } = props;
+import axiosInstance from "../config/axiosInstance";
+
+const NotificationPopup = ({ bindToggle, bindPopper, popupState }) => {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const getUserNotifications = async () => {
+      try {
+        const response = await axiosInstance.get(
+          "http://localhost:8080/api/v1/users/allEvents"
+        );
+        const eventData = response.data; // Assuming response.data is an array of event objects
+        const eventTitles = eventData.map((event) => event.title);
+        const newNotifications = eventTitles.map((title, index) => ({
+          id: index + 1,
+          message: `New event: ${title}`,
+        }));
+        setNotifications(newNotifications);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getUserNotifications();
+  }, []);
 
   return (
     <div>
@@ -20,13 +42,15 @@ const NotificationPopup = (props) => {
           <ClickAwayListener onClickAway={popupState.close}>
             <Fade {...TransitionProps} timeout={350}>
               <StyledNotificationPopup>
-                <List sx={{ p: 2, maxWidth: 200 }}>
-                  {notifications.map((notification) => (
-                    <ListItem key={notification.id}>
-                      <ListItemText primary={notification.message} />
-                    </ListItem>
-                  ))}
-                </List>
+                {notifications.length > 0 ? (
+                  <ul>
+                    {notifications.map((notification) => (
+                      <li key={notification.id}>{notification.message}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No new notifications</p>
+                )}
               </StyledNotificationPopup>
             </Fade>
           </ClickAwayListener>
