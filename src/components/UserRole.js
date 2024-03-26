98421f3a4e4b9f0ca58d6d63fe0fdaef521cf0e1
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-27 01:10:57 +0800

    Implement changing of roles by an admin

--- src/components/UserRole.js
+++ src/components/UserRole.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
@@ -6,6 +6,12 @@ import Divider from "@mui/material/Divider";
 import { Grid } from "@mui/material";
 import { Message } from "./styles/Request.styled";
 import Rolepopper from "./RolePopper";
+import InputLabel from "@mui/material/InputLabel";
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import axiosInstance from "../config/axiosInstance";
 
 const style = {
 	p: 0,
@@ -20,57 +26,117 @@ const style = {
 };
 
 const UserRole = (props) => {
-	const { requests } = props;
+	const { requests, updateUsers } = props;
+	const [selected] = useState("");
+	const [shownUsers, setShownUsers] = useState(requests);
+
+	const changeUserRole = async (newRole, userId) => {
+		try {
+			await axiosInstance.post(`/admins/${userId}/${newRole}`);
+			await updateUsers();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleChange = (event) => {
+		const selectedRole = event.target.value;
+		if (selectedRole !== "ALL") {
+			const filtered = filterUsersByAuthority(selectedRole);
+			setShownUsers(filtered);
+		} else {
+			setShownUsers(requests);
+		}
+	};
+
+	function filterUsersByAuthority(selectedAuthority) {
+		return requests.filter((user) => {
+			return (
+				user.authorities &&
+				user.authorities.some(
+					(authority) => authority.authority === selectedAuthority
+				)
+			);
+		});
+	}
 
 	return (
-		<List sx={style} aria-label="guest requests">
-			<ListItem
+		<div>
+			<Box
 				sx={{
-					borderTopLeftRadius: "10px",
-					borderTopRightRadius: "10px",
-					padding: "10px",
+					width: "100%",
+					display: "flex",
+					justifyContent: "flex-end",
 				}}
 			>
-				<Grid container spacing={2} alignItems="center">
-					<Grid item xs={10}>
-						<ListItemText primary="Name" />
+				<FormControl sx={{ m: 1, minWidth: 120 }}>
+					<InputLabel id="demo-simple-select-helper-label">Role</InputLabel>
+					<Select
+						labelId="demo-simple-select-helper-label"
+						id="demo-simple-select-helper"
+						label="Role"
+						value={selected}
+						onChange={handleChange}
+					>
+						<MenuItem value="ALL">
+							<em>All</em>
+						</MenuItem>
+						<MenuItem value="USER">User</MenuItem>
+						<MenuItem value="ORGANIZER">Organizer</MenuItem>
+						<MenuItem value="ADMIN">Admin</MenuItem>
+					</Select>
+				</FormControl>
+			</Box>
+			<List sx={style} aria-label="guest requests">
+				<ListItem
+					sx={{
+						borderTopLeftRadius: "10px",
+						borderTopRightRadius: "10px",
+						padding: "10px",
+					}}
+				>
+					<Grid container spacing={2} alignItems="center">
+						<Grid item xs={10}>
+							<ListItemText primary="Name" />
+						</Grid>
+						<Grid item xs={2}>
+							<ListItemText primary="Actions" />
+						</Grid>
 					</Grid>
-					<Grid item xs={2}>
-						<ListItemText primary="Actions" />
-					</Grid>
-				</Grid>
-			</ListItem>
-			{requests.length === 0 ? (
-				<div>
-					<Divider component="li" />
-					<Message>Request Empty</Message>
-				</div>
-			) : (
-				<>
-					<Divider component="li" />
-					{requests.map((request) => (
-						<React.Fragment key={request.userId}>
-							<ListItem>
-								<Grid container spacing={2} alignItems="center">
-									<Grid item xs={10}>
-										<ListItemText primary={request.username} />
+				</ListItem>
+				{shownUsers.length === 0 ? (
+					<div>
+						<Divider component="li" />
+						<Message>Request Empty</Message>
+					</div>
+				) : (
+					<>
+						<Divider component="li" />
+						{shownUsers.map((request) => (
+							<React.Fragment key={request.userId}>
+								<ListItem>
+									<Grid container spacing={2} alignItems="center">
+										<Grid item xs={10}>
+											<ListItemText primary={request.username} />
+										</Grid>
+										<Grid item xs={2}>
+											<Rolepopper
+												role={request.authorities[0].authority}
+												userId={request.userId}
+												changeUserRole={changeUserRole}
+											/>
+										</Grid>
 									</Grid>
-									<Grid item xs={2}>
-										<Rolepopper
-											role={request.authorities[0].authority}
-											userId={request.userId}
-										/>
-									</Grid>
-								</Grid>
-							</ListItem>
-							{requests.indexOf(request) !== requests.length - 1 && (
-								<Divider component="li" />
-							)}
-						</React.Fragment>
-					))}
-				</>
-			)}
-		</List>
+								</ListItem>
+								{requests.indexOf(request) !== requests.length - 1 && (
+									<Divider component="li" />
+								)}
+							</React.Fragment>
+						))}
+					</>
+				)}
+			</List>
+		</div>
 	);
 };
 
