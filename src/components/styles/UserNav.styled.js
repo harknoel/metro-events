611daedd8533ee59f5c1bc58fe4bfb1c4d21a366
Hark Noel Bontilao <91307472+harknoel@users.noreply.github.com>
2024-03-27 21:22:13 +0800

    Revert "Pull request update"

--- src/components/styles/UserNav.styled.js
+++ src/components/styles/UserNav.styled.js
@@ -1,83 +1,60 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
 export const StyledUserNav = styled.header`
-  background-color: #f7f6fc;
-`;
-
-export const MarginCenter = styled.div`
-  width: 1250px;
-  // max-width: 100%;
-  // padding: 10px 20px;
-  // margin: 0 auto;
+	background-color: #f7f6fc;
 `;
 
 export const Header = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 `;
 
 export const Logo = styled.img`
-  width: 120px;
-  height: 60px;
-  margin-top: 10px;
+	width: 100px;
+	height: 50px;
 `;
 
 export const Nav = styled.nav`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  gap: 10px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	gap: 10px;
 `;
 
 export const NavLink = styled(Link)`
-  text-decoration: none;
-  color: #6462f1;
-  font-family: "Poppins", sans-serif;
-  transition: box-shadow 0.3s ease; /* Add transition for box-shadow change */
-  padding: 3px; /* Add padding for better hover area */
-
-  &:hover {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Add box-shadow effect on hover */
-  }
+	text-decoration: none;
+	color: #6462f1;
+	font-family: "Poppins", sans-serif;
+	&:hover {
+		text-decoration: underline;
+		color: #433dcf;
+	}
 `;
 
 export const CreateEventButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #6462f1;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  decoration: none;
-  font-weight: bold;
-  transition: background-color 0.3s;
-  &:hover {
-    background-color: #7c7af5;
-  }
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #6462f1;
+	color: white;
+	border: none;
+	padding: 10px 20px;
+	border-radius: 5px;
+	cursor: pointer;
+	font-size: 16px;
+	decoration: none;
+	font-weight: bold;
+	transition: background-color 0.3s;
+	&:hover {
+		background-color: #7c7af5;
+	}
 `;
 
 export const OrganizerContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-`;
-
-// Styled SearchBar component
-export const SearchBar = styled.input`
-  width: 300px;
-  margin: 0 10px;
-  padding: 8px 10px; // Add padding for better appearance
-  border-radius: 20px;
-  border: 1px solid #ccc;
-  font-family: "Poppins", sans-serif; // Optional: Apply font family
-  font-size: 16px; // Optional: Apply font size
-  margin-right: 500px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
 `;
--- src/pages/UserEvents.js
+++ src/pages/UserEvents.js
@@ -1,52 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Event from "./Event";
 import UserNav from "../components/UserNav";
-
-import CenterDiv from "../components/styles/CenterDiv";
+import Container from "../components/styles/Container.styled";
 import { CardContainer } from "../components/styles/UserEvents.styled";
 import axiosInstance from "../config/axiosInstance";
 
 const UserEvents = () => {
-  const [open, setOpen] = useState(false);
-  const [events, setEvents] = useState();
+	const [open, setOpen] = useState(false);
+	const [events, setEvents] = useState();
 
-  const getAllUserEvents = async () => {
-    const username = localStorage.getItem("username");
-    try {
-      const response = await axiosInstance.get(
-        `http://localhost:8080/api/v1/users/events/${username}`
-      );
-      setEvents(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const getAllUserEvents = async () => {
+		const username = localStorage.getItem("username");
+		try {
+			const response = await axiosInstance.get(
+				`http://localhost:8080/api/v1/users/events/${username}`
+			);
+			setEvents(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    getAllUserEvents();
-  });
+	useEffect(() => {
+		getAllUserEvents();
+	});
 
-  return (
-    <div>
-      <UserNav />
-      <CenterDiv>
-        <h1>My Joined Events</h1>
-        <CardContainer>
-          {events &&
-            events.map((event) => (
-              <Event
-                key={event.eventId}
-                event={event}
-                open={open}
-                setOpen={setOpen}
-                showJoin={true}
-                showManage={true}
-              />
-            ))}
-        </CardContainer>
-      </CenterDiv>
-    </div>
-  );
+	return (
+		<div>
+			<UserNav />
+			<Container>
+				<h1>My Joined Events</h1>
+				<CardContainer>
+					{events &&
+						events.map((event) => (
+							<Event
+								key={event.eventId}
+								event={event}
+								open={open}
+								setOpen={setOpen}
+								showJoin={true}
+								showManage={true}
+							/>
+						))}
+				</CardContainer>
+			</Container>
+		</div>
+	);
 };
 
 export default UserEvents;
