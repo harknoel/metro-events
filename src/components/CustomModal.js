856e37f036b5fbbb76c337fc0870adf7c080023c
Topitops69 <topitops123@gmail.com>
2024-03-28 11:33:27 +0800

    made changes

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -160,7 +160,7 @@ const CustomModal = (props) => {
                       {liked ? <Favorite color="error" /> : <FavoriteBorder />}
                     </IconButton>
                     <Typography variant="body1">
-                      {/* Made changes */}
+                      {/* Made changes because of error*/}
                       {/* <strong>{event.upvoteList.length} </strong>Upvote */}
                     </Typography>
                   </Box>
