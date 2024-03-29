5dbd740b4a420c01f6c5d0ba4fa1d33fb51d3ab4
luna-wee <83816898+herald-noel@users.noreply.github.com>
2024-03-29 21:00:20 +0800

    Change names according to tab

--- src/components/AdminUserRequest.js
+++ src/components/AdminUserRequest.js
@@ -10,83 +10,83 @@ import UserRole from "./UserRole";
 import { useState, useEffect } from "react";
 
 export default function AdminUserRequest() {
-	const [value, setValue] = useState("1");
-	const [requests, setRequests] = useState([]);
-	const [allUsers, setAllUsers] = useState([]);
+  const [value, setValue] = useState("1");
+  const [requests, setRequests] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
 
-	useEffect(() => {
-		getRequests();
-		getAllUsers();
-	}, []);
+  useEffect(() => {
+    getRequests();
+    getAllUsers();
+  }, []);
 
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-	};
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-	const getAllUsers = async () => {
-		try {
-			const response = await axiosInstance.get("/users");
-			setAllUsers(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const getAllUsers = async () => {
+    try {
+      const response = await axiosInstance.get("/users");
+      setAllUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const updateUsers = async () => {
-		try {
-			await getAllUsers();
-			return allUsers;
-		} catch {}
-	};
+  const updateUsers = async () => {
+    try {
+      await getAllUsers();
+      return allUsers;
+    } catch {}
+  };
 
-	const getRequests = async () => {
-		try {
-			const response = await axiosInstance("admins/organizer/requests");
-			setRequests(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const getRequests = async () => {
+    try {
+      const response = await axiosInstance("admins/organizer/requests");
+      setRequests(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const acceptRequest = async (id) => {
-		console.log(id);
-		try {
-			await axiosInstance.post("admins/organizer/accept", id);
-			getRequests();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const acceptRequest = async (id) => {
+    console.log(id);
+    try {
+      await axiosInstance.post("admins/organizer/accept", id);
+      getRequests();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const declineRequest = async (id) => {
-		try {
-			await axiosInstance.post("admins/organizer/decline", id);
-			getRequests();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const declineRequest = async (id) => {
+    try {
+      await axiosInstance.post("admins/organizer/decline", id);
+      getRequests();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<Box sx={{ width: "100%", typography: "body1" }}>
-			<TabContext value={value}>
-				<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-					<TabList onChange={handleChange} aria-label="lab API tabs example">
-						<Tab label="Users" value="1" />
-						<Tab label="Requests" value="2" />
-					</TabList>
-				</Box>
-				<TabPanel value="1">
-					<RequestList
-						requests={requests}
-						acceptRequest={acceptRequest}
-						declineRequest={declineRequest}
-					/>
-				</TabPanel>
-				<TabPanel value="2">
-					<UserRole requests={allUsers} updateUsers={updateUsers} />
-				</TabPanel>
-			</TabContext>
-		</Box>
-	);
+  return (
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Requests" value="1" />
+            <Tab label="Users" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <RequestList
+            requests={requests}
+            acceptRequest={acceptRequest}
+            declineRequest={declineRequest}
+          />
+        </TabPanel>
+        <TabPanel value="2">
+          <UserRole requests={allUsers} updateUsers={updateUsers} />
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
 }
